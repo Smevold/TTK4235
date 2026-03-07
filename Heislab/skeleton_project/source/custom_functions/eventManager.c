5125393abe73397ba1727e0f7179e3d0175134ad
August Hauge Smevold <august.hauge.smevold@gmail.com>
2026-03-07 18:38:01 +0100

    Flyttet funksjoner til eventManager

--- Heislab/skeleton_project/source/custom_functions/eventManager.c
+++ Heislab/skeleton_project/source/custom_functions/eventManager.c
@@ -1,3 +1,22 @@
 #include "eventManager.h"
 
 // Clear funksjon - tømmer historikken og setter nextFloor lik -1
+
+int ioTest_getNextFloor() {
+    for(int f = 0; f < N_FLOORS; f++) {
+        for(int b = 0; b < N_BUTTONS; b++) {
+            int btnPressed = elevio_callButton(f,b);
+            elevio_buttonLamp(f, b, btnPressed);
+            printf("f: %d, b: %d, btn: %d   |   \n", f, b, btnPressed);
+            if(btnPressed == 1) {
+                return f;
+            }
+        }
+    }
+    return -1;
+}
+
+int ioTest_getCurrentFloor() {
+    int floor = elevio_floorSensor();
+    return floor;
+}
--- Heislab/skeleton_project/source/custom_functions/eventManager.h
+++ Heislab/skeleton_project/source/custom_functions/eventManager.h
@@ -1,3 +1,21 @@
 #pragma once
+#include <stdio.h>
+#include "../driver/elevio.h"
 
+/**
+* @file
+* @brief Denne skal følge med på knappene og bruke en algoritme til å finne ut hvilken etasje som er den neste
+*/
 
+/**
+* @brief Sjekker hvilke knakker som blir trykket
+* @param[out] f Hvilken etasje som har blitt trykket på, der @p f er nullindeksert
+*
+* @warning Dersom @p f er lik -1, skal dette telle som at ingen etasjeknapp er trykket på
+*/
+int ioTest_getNextFloor();
+
+/**
+* @brief Sjekker hvilken etasje heisen er i
+*/
+int ioTest_getCurrentFloor();
