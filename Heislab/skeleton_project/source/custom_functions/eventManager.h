97a0ff9527312cb82519d53680588bf36497446b
August Hauge Smevold <august.hauge.smevold@gmail.com>
2026-03-11 12:38:26 +0100

    em_getNextFloor finner nå etasjer under/over den i tilsvarende kø som retningen

--- Heislab/skeleton_project/source/custom_functions/eventManager.h
+++ Heislab/skeleton_project/source/custom_functions/eventManager.h
@@ -11,7 +11,7 @@
 * @brief Sletter hele kø-en til Event Manageren
 * @param[in] em_queue Peker til køen som programmet bruker til å finne neste etasje
 */
-void em_clear(int *em_queueUp, int*em_queueDown);
+void em_clear(int* em_queueUp, int* em_queueDown);
 
 /**
 * @brief Sjekker hvilke knakker som blir trykket
@@ -28,7 +28,7 @@ void em_checkBtnPressed(int* floor, int* btn);
 *
 * @warning Dersom heisen er mellom etasjer, vil den ikke oppdatere verdien
 */
-void em_getCurrentFloor(int *currentFloor);
+void em_getCurrentFloor(int* em_currentFloor);
 
 /**
 * @brief Fjerner currentFloor fra heiskøen i begge retninger. currentFloor trenger ikke oppdateres før denne funksjonen kalles.
@@ -36,7 +36,7 @@ void em_getCurrentFloor(int *currentFloor);
 * @param[in] em_queueUp Pass-by-reference: Heiskøen i oppover-retning
 * @param[in] em_queueDown Pass-by-reference: Heiskøen i nedover-retning
 */
-void em_clearCurrentFloor(int currentFloor, int* em_queueUp, int* em_ququeDown);
+void em_clearCurrentFloor(int* em_currentFloor, int* em_queueUp, int* em_queueDown);
 
 /**
 * 
@@ -47,9 +47,9 @@ void em_updateQueues(int* em_queueUp, int* em_queueDown);
 * @brief Genererer etasjerekkefølge basert på 
 * @param[in] nextFloor nextFloor er en peker til forrige etasje heisen var på vei til
 */
-void em_getNextFloor(int *nextFloor, int* em_queueUp, int* em_queueDown, int* motorDir, int* currentFloor);
+void em_getNextFloor(int *em_nextFloor, int* em_queueUp, int* em_queueDown, int* motorDirection, int* em_currentFloor);
 
 /**
 * @brief Skriver kø og nextFloor til terminalen for debugging
 */
-void em_printQueue(int* nextFloor, int* em_queueUp, int* em_queueDown);
+void em_printQueue(int* em_nextFloor, int* em_queueUp, int* em_queueDown);
