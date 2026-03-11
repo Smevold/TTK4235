eb4818c1f239065f1d3f18f0912bfb37472279d3
August Hauge Smevold <august.hauge.smevold@gmail.com>
2026-03-11 18:22:27 +0100

    fikset noe doxygen for lys

--- Heislab/skeleton_project/source/custom_functions/smallerFunctions.h
+++ Heislab/skeleton_project/source/custom_functions/smallerFunctions.h
@@ -26,7 +26,7 @@ void mDirStop(int *MotorDirection);
 
 /**
 * @brief Skrur av alle bestillingslysene i gjeldende etasje
-* @param[in] Pass-by-reference: gjeldende etasje
+* @param[in] em_currentFloor Pass-by-reference: gjeldende etasje
 */
 void lights_floorOffCurrent(int* em_currentFloor);
 
