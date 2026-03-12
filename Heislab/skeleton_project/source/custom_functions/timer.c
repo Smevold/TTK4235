a03fc136f6908962074f0b83559a4fb99f827936
August Hauge Smevold <august.hauge.smevold@gmail.com>
2026-03-12 15:32:11 +0100

    endret navn på timer_getTime

--- Heislab/skeleton_project/source/custom_functions/timer.c
+++ Heislab/skeleton_project/source/custom_functions/timer.c
@@ -2,19 +2,19 @@
 #include <sys/time.h>
 // #include <stdio.h>
 
-static double get_wall_time(void){
+static double timer_getTime(void){
     struct timeval time;
     gettimeofday(&time, NULL);
     return (double)time.tv_sec + (double)time.tv_usec * .000001;
 }
 
 void timer_start(clock_t* pt_start,clock_t* pt_end){
-    *pt_start = get_wall_time();
+    *pt_start = timer_getTime();
     *pt_end = *pt_start + 3;
 }
 
 int timer_isTimeOut(clock_t *pt_start,clock_t *pt_end){
-    *pt_start = get_wall_time();
+    *pt_start = timer_getTime();
     if (*pt_end <= *pt_start){
         return 1;
     }
